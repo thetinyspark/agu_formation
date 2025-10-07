b3abf7ced1f20270e8466a40e81b1d73abc729d2
BlackB34rd <contact@the-tiny-spark.com>
2025-10-07 11:55:19 +0200

    add navbar + TP n°1

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -11,56 +11,12 @@ import { environment } from '../../../environments/environment';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyApp';
+  
+  public title = 'MyApp';
 
   constructor(){
-
-    let age:number = 39;
+    let age:number = 40;
     let name:string = "Nicolas";
-    let title = `
-    Bonjour 
-    je 
-    m'appelle 
-    ${name} 
-    et 
-    j'ai ${age} ans
-    `;
-
-    this.title = title.split(`\n`).join("<br />");
-
-    class Test{
-      private _myfunc:Function;
-      public name:string = "Test";
-      constructor( func:Function){
-        this._myfunc = func;
-      }
-
-      execute(){
-        this._myfunc();
-      }
-    }
-
-
-    // const myFunc1 = function (this: any){
-    //   console.log(this);
-    // };
-
-    // const myFunc2 = ()=>{
-    //   console.log(this);
-    // };
-
-
-    this.introduceMySelf = this.introduceMySelf.bind(this);
-
-    const test1 = new Test(this.introduceMySelf);
-    const test2 = new Test(this.introduceMySelf);
-
-    test1.execute();
-    test2.execute();
+    this.title = `Bonjour je m'appelle ${name} et j'ai ${age} ans`;
   }
-
-  introduceMySelf(age:number = 10, name:string = "Toto"){
-    // this.title = `Bonjour je m'appelle ${name} et j'ai ${age} ans`;
-    console.log(this.title);
-  };
 }
