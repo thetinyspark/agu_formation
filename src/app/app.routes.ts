22bd328fce67721ebaf2a891de77d0eeb7fbbb89
BlackB34rd <contact@the-tiny-spark.com>
2025-10-07 13:50:55 +0200

    add multi components add route config add navbar + routerlink add TP n°2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routeConfig: Routes = [
-  // {
-  //   path: '',
-  //   component: HomeComponent,
-  //   title: 'Home page'
-  // }
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page'
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'My Cart'
+  }
 ];
 
 export default routeConfig;
