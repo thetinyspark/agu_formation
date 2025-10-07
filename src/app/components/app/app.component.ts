3be19da8ff65e947bc9bb150de494d5a4308dcbc
BlackB34rd <contact@the-tiny-spark.com>
2025-10-07 12:27:27 +0200

    correction TP n°1

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
   public title = 'MyApp';
 
   constructor(){
