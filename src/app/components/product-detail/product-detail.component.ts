c13aba6b565a0647dda1c3b615d4174da9ad7713
BlackB34rd <contact@the-tiny-spark.com>
2025-10-08 12:11:04 +0200

    correction TP N°8

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,34 +1,34 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CatalogService } from '../../services/catalog.service';
+import { Product } from '../../models/product';
+import { firstValueFrom } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
   private _activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   private _catalog:CatalogService = inject(CatalogService);
+  public currentProduct:Product|null = null;
 
-  public ngOnInit():void{
+  public async ngOnInit(){
 
-    // l'objet activatedRoute permet d'obtenir des infos 
-    // sur la route en cours
-    this._activatedRoute.paramMap.subscribe( 
+    // l'objet activatedRoute permet d'obtenir des infos sur la route en cours
+    const map:ParamMap =  await firstValueFrom( this._activatedRoute.paramMap );
 
-      async (map:ParamMap)=>{
-        // une fois que l'on a obtenu l'objet ParamMap promis
-        // on cherche à en extraite le paramètre id contenu dans la route
-       const id:number = parseInt(map.get("id") || "-1");
+    // une fois que l'on a obtenu l'objet ParamMap promis
+    // on cherche à en extraite le paramètre id contenu dans la route
+    const id:number = parseInt(map.get("id") || "-1");
 
-       // puis on récupère le produit correspondant
-       const product = await this._catalog.getProductById(id);
-       console.log(product);
-      }
-
-    )
+    // puis on récupère le produit correspondant
+    this.currentProduct = await this._catalog.getProductById(id);
+       
+      
   }
 }
