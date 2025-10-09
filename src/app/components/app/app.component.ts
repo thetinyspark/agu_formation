7fa940dbb839a86f261aec3228018b03f6163264
BlackB34rd <contact@the-tiny-spark.com>
2025-10-09 12:23:58 +0200

    add correction TP N°15

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import { Observable, Subject } from 'rxjs';
+import { interval, Observable, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,30 @@ export class AppComponent {
   public title = 'My Game Video Store';
 
   constructor(){
-    const subj = new Subject<string>();
+    // replaysubject possède un historique de diffusion
+    // il rejoue toutes les données précédemment diffusée avant
+    // la souscription
+    const subj = new ReplaySubject<string>();
+
+    const sub1 = interval(1000).subscribe(
+      (index:number)=>{
+        subj.next(`News N°${index}`);
+      }
+    );
     
-    subj.subscribe( console.log );
-    subj.next("Katia");
-    subj.next("Lucas");
+    setTimeout( ()=>subj.subscribe( console.log ), 3000 );
+    setTimeout( ()=>subj.subscribe( (value:string)=>{
+      console.log("Cette news a été diffusée : "+value)
+    } ), 5000 );
+
+    // on arrête la diffusion au bout de 10s et on complete le flux de subj
+    setTimeout( 
+      ()=>{
+        sub1.unsubscribe();
+        subj.complete();
+      }, 
+      10000
+    )
     
   }
 }
