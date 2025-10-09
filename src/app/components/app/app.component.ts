fd02aecae1bb84ca832655ab8f43267250bbf130
BlackB34rd <contact@the-tiny-spark.com>
2025-10-09 13:51:14 +0200

    add combineLatest + forkJoin

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import { interval, Observable, ReplaySubject, Subject } from 'rxjs';
+import { combineLatest, forkJoin, interval, Observable, of, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,30 +16,38 @@ export class AppComponent {
   public title = 'My Game Video Store';
 
   constructor(){
-    // replaysubject possède un historique de diffusion
-    // il rejoue toutes les données précédemment diffusée avant
-    // la souscription
-    const subj = new ReplaySubject<string>();
 
-    const sub1 = interval(1000).subscribe(
-      (index:number)=>{
-        subj.next(`News N°${index}`);
+    const obs1 = new Subject();
+    const obs2 = new Subject();
+    const obs3 = new Subject();
+    // forkjoin attend que les flux soient complétés pour diffuser
+    // combineLatest lui, diffuse à chaque fois, les dernières valeurs diffusées
+    // dans chaque canal séparé. 
+
+    // ATTENTION: les deux demandent que chacun des canaux ait diffusé au moins UNE donnée
+    combineLatest({
+      price: obs1, 
+      vat: obs2, 
+      discount: obs3
+    }).subscribe( 
+      (obj:any)=>{
+        const price = ( (obj.price - (obj.price * obj.discount ) ) * (1 + obj.vat) ); 
+        console.log(price);
       }
     );
-    
-    setTimeout( ()=>subj.subscribe( console.log ), 3000 );
-    setTimeout( ()=>subj.subscribe( (value:string)=>{
-      console.log("Cette news a été diffusée : "+value)
-    } ), 5000 );
 
-    // on arrête la diffusion au bout de 10s et on complete le flux de subj
-    setTimeout( 
-      ()=>{
-        sub1.unsubscribe();
-        subj.complete();
-      }, 
-      10000
-    )
+
+    // premier jeu de données diffusé
+    obs1.next(100);
+    obs2.next(0.05);
+    obs3.next(0.2);
+
+
+    // maintenanto on peut faire varier indépendamment chacune des données, cela 
+    // aura pour effet de redéclencher le subscribe
+
+    obs1.next(200);
+    obs2.next(0.1);
     
   }
 }
