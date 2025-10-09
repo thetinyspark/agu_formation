01af6e9f054c66483991b3fa5fca99294398b679
BlackB34rd <contact@the-tiny-spark.com>
2025-10-09 11:56:43 +0200

    add Subject example

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import { delay, interval, map, Observable, of } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,47 +16,11 @@ export class AppComponent {
   public title = 'My Game Video Store';
 
   constructor(){
-    // of permet de créer un observable à partir de données synchrones
-    // pipe permet de rediriger chacune des valeurs diffusée par l'observable
-    // vers un opérateur (ici map). Cela résulte en un nouvel observable.
-    const obs1 = of(100,200,300,400).pipe(
-      map(
-        (value:number, index:number)=>{
-          const tva = 0.2; // 20% TVA
-          return value * ( 1+tva );
-        }
-      )
-    ).pipe( 
-      // delay ralentit la diffusion des données de x ms
-      delay(1000)
-    );
-
-    const sub = obs1.subscribe(
-      {
-        next: (data:number)=>{
-          console.log(data);
-        }, 
-        complete: ()=>{
-          console.log("complete");
-        }, 
-        error: (reason:any)=>{
-          console.log("reason", reason);
-        }
-      }
-    );
-
-    // interval permet de créer un observable qui diffuse quelque chose
-    // toutes les x ms (par défaut, c'est un entier qui s'incrémente)
-    const obs2 = interval(1000);
-    const sub2 = obs2.subscribe(
-      (index:number)=>{
-        const nums = [100,200,300,400];
-        console.log(nums[index%4]);
-      }
-    ); 
-
-    sub2.unsubscribe();
-
-
+    const subj = new Subject<string>();
+    
+    subj.subscribe( console.log );
+    subj.next("Katia");
+    subj.next("Lucas");
+    
   }
 }
