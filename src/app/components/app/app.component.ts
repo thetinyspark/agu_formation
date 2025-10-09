629150a451ebc3686f1c39a762f80453bda57d05
BlackB34rd <contact@the-tiny-spark.com>
2025-10-09 15:56:45 +0200

    add correction TP N°17

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,52 +1,40 @@
-import { Component, effect, Signal, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import { combineLatest, forkJoin, interval, map, Observable, of, ReplaySubject, Subject } from 'rxjs';
+import {
+  combineLatest,
+  forkJoin,
+  interval,
+  map,
+  Observable,
+  of,
+  ReplaySubject,
+  Subject,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-
   public title = 'My Game Video Store';
-  public s1 = signal<boolean>(true);
-  public s2 = signal<string>("");
+  public price = signal<number>(100);
+  public vat = signal<number>(20);
+  public total = computed( 
+    ()=>{
+      return this.price() * (1 + this.vat() / 100);
+    }
+  );
 
-  constructor(){
-
-
-    effect(
-      ()=>{
-        const isFinished = this.s1();
-        let prefix = "";
-        if( !isFinished ){
-          prefix = "Loading: ";
-        }
-        this.title = prefix + this.s2();
-      }
-    )
-
-    this.s2.set("");
-
+  public upVAT() {
+    this.vat.set(this.vat() + 1);
   }
 
-  public onClick(){
-    // on déclenche notre requête
-    this.s1.set(false);
-    this.s2.set("Produits")
-
-    setTimeout(
-      ()=>{
-        // on a reçu la requête
-        this.s1.set(true);
-        this.s2.set("done");
-      }, 
-      5000
-    );
+  public upPrice() {
+    this.price.set(this.price() + 10);
   }
 }
