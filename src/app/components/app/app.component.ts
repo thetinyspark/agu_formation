36cc2fd438fe50d408e5e4d9e6e1e2c2adfce5a8
BlackB34rd <contact@the-tiny-spark.com>
2025-10-09 10:34:04 +0200

    add Observable example

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +16,43 @@ export class AppComponent {
   public title = 'My Game Video Store';
 
   constructor(){
+    const obs1 = new Observable<number>( 
+      (observer)=>{
+        observer.next(10);
+         setTimeout( 
+          ()=>{
+            observer.next(100);
+          }, 
+          1000
+        )
+
+        // complete le flux de données, impossible de pousser
+        // d'avantage de données à l'intérieur
+
+
+        // observer.error("404 not found");
+
+        setTimeout( 
+          ()=>{
+            observer.complete();
+          }, 
+          5000
+        )
+      }
+    );
+
+    obs1.subscribe(
+      {
+        next: (data:number)=>{
+          console.log(data);
+        }, 
+        complete: ()=>{
+          console.log("complete");
+        }, 
+        error: (reason:any)=>{
+          console.log("reason", reason);
+        }
+      }
+    );
   }
 }
